ffd621fd229fa3f16607dedeacc2a6693dd91047
Travis Vela <jtravisvela@gmail.com>
2019-02-07 10:42:10 -0600

    add inline js exercise, add external js exercise

--- js/external.js
+++ js/external.js
@@ -1,6 +1,9 @@
+"use strict";
+
+
 console.log('Hello from external JavaScript');
 
-// alert('Welcome to my website!');
+// alert('Welcome to my Website!');
 //
 // var favColor = prompt('What is your favorite color?');
 // alert(favColor + ' is my favorite color too!');
@@ -9,34 +12,35 @@ console.log('\n');
 console.log('- - - - - - - -');
 console.log('\n');
 
-// var mermaid = prompt('How many days do you want to rent Little Mermaid?');
-// var bear = prompt('How many days do you want to rent Brother Bear?');
-// var herc = prompt('How many days do you want to rent Hercules?');
-//
-// var total = (mermaid * 3) + (bear * 3) + (herc * 3);
-// alert('Total amount of rental fees is $' + total + '.');
-//
-// console.log('\n');
-// console.log('- - - - - - - -');
-// console.log('\n');
-//
-// var google = 400;
-// var amazon = 380;
-// var facebook = 350;
-//
-// var googlePay = prompt('How many hours worked for Google this week?');
-// var amazonPay = prompt('How many hours worked for Amazon this week?');
-// var facebookPay = prompt('How many hours worked for Facebook this week?');
-// var totalPay = alert('Your total pay is $' + ((google * googlePay) + (amazon * amazonPay) + (facebook *facebookPay)) + ' this week.');
+var mermaid = prompt('How many days do you want to rent Little Mermaid?');
+var bear = prompt('How many days do you want to rent Brother Bear?');
+var herc = prompt('How many days do you want to rent Hercules?');
+
+var total = (mermaid * 3) + (bear * 3) + (herc * 3);
+alert('Total amount of rental fees is $' + total + '.');
 
 console.log('\n');
 console.log('- - - - - - - -');
 console.log('\n');
 
-var isClassFull = false;
+var google = 400;
+var amazon = 380;
+var facebook = 350;
+
+var googlePay = prompt('How many hours worked for Google this week?');
+var amazonPay = prompt('How many hours worked for Amazon this week?');
+var facebookPay = prompt('How many hours worked for Facebook this week?');
+var totalPay = alert('Your total pay is $' + ((google * googlePay) + (amazon * amazonPay) + (facebook *facebookPay)) + ' this week.');
+
+console.log('\n');
+console.log('- - - - - - - -');
+console.log('\n');
+
+var isClassFull = prompt('Is the class full? True or False.');
+var classFull = isClassFull === 'true';
 var doesScheduleConflict = prompt('Does this conflict with your current schedule? True or False.');
-var noConflict = doesScheduleConflict.toLowerCase() === 'true';
-var canEnroll = !isClassFull && !noConflict;
+var noConflict = doesScheduleConflict.toLowerCase() === 'false';
+var canEnroll = !classFull && noConflict;
 alert('Enrolled: ' + canEnroll);
 
 console.log('\n');
@@ -52,3 +56,4 @@ var isExpired = expired === 'true';
 var offer = ((items > 2) || isMember) && !isExpired;
 
 alert('Offer good? ' + offer);
+
