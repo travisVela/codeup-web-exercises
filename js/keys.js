e7346079995391dbbd8edb7b2b7fa6859eabb1ca
Travis Vela <jtravisvela@gmail.com>
2019-02-25 16:55:26 -0600

    update .ignore

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+
+
+var mapboxToken = 'pk.eyJ1IjoidHJhdmlzdmVsYSIsImEiOiJjanNrbWxpZzUwYXY5M3lrOWE1MzM1ajdsIn0.Egv15ec1oax3P1aOkahDJw';
+
+var googleMapsAccessKey = 'AIzaSyBV-aBbUM6lCiK47fgkKe8OXAj6AuCMdQU';
