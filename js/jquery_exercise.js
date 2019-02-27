7377c5208c527a13c33079defd3d713a6ea764ca
Travis Vela <jtravisvela@gmail.com>
2019-02-27 13:18:59 -0600

    add multiple selectors section of exercise

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -2,9 +2,10 @@
 
 $(document).ready(function() {
     $('li').css('font-size', '20px');
-    $('h1').css('background-color', '#ffff0050');
-    $('p').css('background-color', '#ffff0050');
-    $('li').css('background-color', '#ffff0050');
+    // $('h1').css('background-color', '#ffff0050');
+    // $('p').css('background-color', '#ffff0050');
+    // $('li').css('background-color', '#ffff0050');
+    $('h1, p, li').css('background-color', '#ffff0050');
     alert($('h1').html());
 });
 
