292411f0bd8e03327390a59e49d1c0761bb231b8
Travis Vela <jtravisvela@gmail.com>
2019-02-28 14:22:46 -0600

    add solutions

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,23 +1,24 @@
 "use strict";
 
 $(document).ready(function() {
-    $('h1').css('cursor', 'pointer');
+
+    $('h1, p, li').css('cursor', 'pointer');
     $('h1').click(function() {
         $(this).css('background-color', '#eee');
     });
 
-    $('p').css('cursor', 'pointer');
+
     $('p').dblclick(function() {
         $(this).css('font-size', '18px');
     });
 
-    $('li').css('cursor', 'hover');
+
     $('li').hover(
         function() {
         $(this).css('color', '#f00');
         },
         function() {
-            $(this).css('color', 'black');
+            $(this).css('color', '');
         })
 });
 
