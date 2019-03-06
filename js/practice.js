ec3878a9c071bf68cff4ba21a8b76c56c5e16e50
Travis Vela <jtravisvela@gmail.com>
2019-03-06 14:40:16 -0600

    add jquery bonus exercises

--- js/practice.js
+++ js/practice.js
@@ -576,3 +576,26 @@ function makeSandwhichObjects(input1, input2) {
 console.log(makeSandwhichObjects(breads, fillings));
 
 
+ console.log('\n');
+ console.log('fizzBuzz');
+ console.log('======');
+ console.log('\n');
+
+ function fizzBuzz(num) {
+
+     for (var i = 1; i <= num; i++) {
+         if (i % 15 === 0) {
+             console.log('fizzBuzz');
+         } else if (i % 5 === 0) {
+             console.log('Buzz');
+         } else if (i % 3 === 0) {
+             console.log('fizz');
+         } else console.log(i);
+     }
+ }
+ var num = Math.floor(Math.random() * 200) + 30;
+ console.log('The random number is ' + num);
+
+ fizzBuzz(num);
+
+
