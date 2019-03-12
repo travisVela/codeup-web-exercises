1cfc1fbe9fa7da6943988c6691649620c93e00f7
Travis Vela <jtravisvela@gmail.com>
2019-03-12 09:48:42 -0500

    add both versions of import/export method

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
+//require
 const added = require('./say-hello.js');
 
 console.log(added.hello());
 console.log(added.add(2,4));
+
+
+//import/export v1
+// import {message} from './say-hello.js';
+//
+// console.log(message());
+
+//import/export v2 w/default
+// import message from './say-hello.js'
+//
+// console.log(message.message());
--- js/say-hello.js
+++ js/say-hello.js
@@ -1,10 +1,23 @@
 'use strict';
 
+//require method
 const hello = () => { return 'This is a nice message!'};
-
 const add = (x, y) => {return x + y};
 
 module.exports = {hello, add};
+
+
+//import/export method v1
+// export const message = () => 'This is a nice message';
+
+//import/export v2 w/default
+
+// const message = () => 'This is a nice message';
+//
+// export default {message};
+
+
+
 
 
 
